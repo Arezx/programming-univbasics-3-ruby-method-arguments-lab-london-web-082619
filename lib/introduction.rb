c68b443c37eb200870c633077638dca4c9585b8b
Keginold Philip <keginoldp@hotmail.com>
2019-08-21 22:56:20 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,6 +6,8 @@ puts "Hi, my name is #{name}."
 
 puts "Hi, my name is #{name} and I am learning to program in #{language}."
 
+puts "Hi, my name is #{name} and I am learning to program in #{language}."
+
 
 
 
